3f5bba350085c8753d7339ab7d6d4ba1e377f8aa
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-11-18 15:07:46 +0700

    add music to game

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,11 +4,11 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Group;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.media.Media;
 
 public class Main extends Application {
     private AnimationTimer animationTimer;
@@ -18,10 +18,15 @@ public class Main extends Application {
     public void start(Stage stage) {
         Group root = new Group();
         Scene scene = new Scene(root, 800, 600, Color.LIGHTSKYBLUE);
+        String bgMusicPath = ClassLoader.getSystemResource("mario.mp3").toString();
+        Media media = new Media(bgMusicPath);
+        MediaPlayer player = new MediaPlayer(media);
+
         stage.setScene(scene);
         stage.setTitle("Poprio");
         stage.setResizable(false);
         stage.show();
+        player.play();
         GameTest m = new GameTest();
         m.createGame(root);
     }
