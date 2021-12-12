a6c9f81123375a2cfdffabd3ec15becf4a259e82
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-12-12 21:28:22 +0700

    feat: cactus initial

---
+++ src/entity/derived/Cactus.java
@@ -0,0 +1,11 @@
+package entity.derived;
+
+import entity.base.Entity;
+import entity.base.Solid;
+
+public class Cactus extends Entity implements Solid {
+    @Override
+    public void blockPlayer(Entity e) {
+
+    }
+}
